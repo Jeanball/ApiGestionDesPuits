c61320411b963b4b46abc846ab6253dc1a4d0098
Jean-Sébastien Durette <jeanball44@hotmail.com>
2022-08-17 13:10:02 -0400

    BD sur JawsDB

--- config/db.config.js
+++ config/db.config.js
@@ -1,11 +1,19 @@
 const mysql = require('mysql2');
 
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     port: 3307,
+//     user: 'admin',
+//     password: '4444',
+//     database: 'puit'
+// });
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    port: 3307,
-    user: 'admin',
-    password: '4444',
-    database: 'puit'
+    host: 'y6aj3qju8efqj0w1.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    port: 3306,
+    user: 'bol4qxgkh2d6994l',
+    password: 'wvxvigxoobmu471u',
+    database: 'ij99rrdgppktzwso'
 });
 
 db.connect(function(error){
